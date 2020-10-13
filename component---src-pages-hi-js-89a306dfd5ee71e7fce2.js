be1625a586e18711b1fc03ba08bed17bca641f50
Ilham Muhammad <ilhammrg@gmail.com>
2020-10-13 19:37:07 +0700

    test gatsby 2

---
+++ component---src-pages-hi-js-89a306dfd5ee71e7fce2.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{Rwyi:function(e,i,t){"use strict";t.r(i);var a=t("q1tI"),n=t.n(a),l=t("Wbzz"),c=t("Bl7J"),o=t("vrFN");i.default=function(){return n.a.createElement(c.a,null,n.a.createElement(o.a,{title:"Hi"}),n.a.createElement("h1",null,"Hiiiiiiiiiiiiiiiiiii"),n.a.createElement("p",null,"Welcome to Hi page"),n.a.createElement(l.Link,{to:"/"},"Go back to the homepage"))}}}]);
+//# sourceMappingURL=component---src-pages-hi-js-89a306dfd5ee71e7fce2.js.map
